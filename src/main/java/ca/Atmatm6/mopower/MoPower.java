328936cc6e76ed250c82965ee9b0bf14fb4fd123
Atmatm6 Yolo <atmatm6000@gmail.com>
2015-02-26 17:41:22 -0600

    CodeStart
    Startiing my code up

---
+++ src/main/java/ca/Atmatm6/mopower/MoPower.java
@@ -0,0 +1,8 @@
+package ca.Atmatm6.mopower;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.modid ,name = Reference.name, version = Reference.version)
+public class MoPower {
+
+}
---
+++ src/main/java/ca/Atmatm6/mopower/Reference.java
@@ -0,0 +1,7 @@
+package ca.Atmatm6.mopower;
+
+public class Reference {
+    public static final String modid = "";
+    public static final String name = "";
+    public static final String version = "";
+}
